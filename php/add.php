a5d14a83a1b155a270f14eec432d6e50d2f33731
kamkam25 <600112417025@bru.ac.th>
2019-07-12 15:34:50 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,17 @@
+<?php
+echo $_POST['user'];
+echo "Hello World !!!";
+
+$name = "Kasinee";
+echo "Miss." . $name;
+echo "<br>";
+
+echo sum(5,3);
+
+function sum($A,$B){
+    return $A + $B;
+}
+?>
+
+//short term, use one line only.
+<?php'My name is Kam'?>
